cd13160c890fe09df7d0f759b9308c1aa9e8064d
ilhamdhiya01 <ulhaqilhamdhiya@gmail.com>
2025-02-18 16:16:21 +0700

    fix(mongo url): statis url

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const connect = await mongoose.connect(process.env.MONGODB_URI);
+    // const connect = await mongoose.connect(process.env.MONGODB_URI);
+    const connect = await mongoose.connect('mongodb+srv://ulhaqilhamdhiya:jc9DK3D4IoetBjQY@cluster0.cu1ui.mongodb.net/chat_db?retryWrites=true&w=majority&appName=Cluster0');
     console.log(`MongoDB connected: ${connect.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
