81b6950501025e2df57c31944081875e3979553a
ilhamdhiya01 <ulhaqilhamdhiya@gmail.com>
2025-02-18 14:39:26 +0700

    feat(prepare): handle prepare for deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,11 @@
-import express from "express";
-import authRouter from "./routes/auth.router.js";
-import messageRouter from "./routes/message.router.js";
-import dotenv from "dotenv";
-import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import path from "path";
+import { connectDB } from "./lib/db.js";
+import authRouter from "./routes/auth.router.js";
+import messageRouter from "./routes/message.router.js";
 
 // Load environment variables
 dotenv.config();
@@ -12,6 +13,7 @@ dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 // Middleware
 app.use(express.json());
@@ -25,6 +27,14 @@ app.use(
 
 app.use("/api/auth", authRouter);
 app.use("/api/message", messageRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL:
+    import.meta.env.VITE_APP_ENV === "development"
+      ? import.meta.env.VITE_API_URL
+      : "/api",
   withCredentials: true,
 });
 
